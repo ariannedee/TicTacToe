817c9f43e6bd1e9638cfd3a1ba7b6a99f63d8afc
ariannedee <ariannedee@gmail.com>
2014-07-30 14:26:16 -0500

    Initial commit with first two user stories

--- src/com/thoughtworks/arianne/tictactoe/Main.java
+++ src/com/thoughtworks/arianne/tictactoe/Main.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) {
-        char[] gameState = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        char[] gameState = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
         TicTacToe ticTacToe = new TicTacToe(System.out, new BufferedReader(new InputStreamReader(System.in)), gameState);
         ticTacToe.start();
     }
