fa711b41a2f0adb9ce1c4bc50aa530a14b91610d
ariannedee <ariannedee@gmail.com>
2014-08-03 12:02:43 -0500

    Updated method names for checking for win.

--- src/com/thoughtworks/arianne/tictactoe/Board.java
+++ src/com/thoughtworks/arianne/tictactoe/Board.java
@@ -30,66 +30,6 @@ public class Board {
         return false;
     }
 
-    private boolean checkForWin(int location, char symbol) {
-        int index = location-1;
-        int row = index/3;
-        int column = index%3;
-
-        boolean win;
-
-        win = checkRow(row, symbol);
-        win = win || checkColumn(column, symbol);
-        if (row == column) {
-            win = win || checkDiagonal1(symbol);
-        }
-        if (row+column == 2) {
-            win = win || checkDiagonal2(symbol);
-        }
-        return win;
-    }
-
-    private boolean checkDiagonal2(char symbol) {
-        int[] indicesToCheck = {2, 4, 6};
-
-        return checkLine(indicesToCheck, symbol);
-    }
-
-    private boolean checkDiagonal1(char symbol) {
-        int[] indicesToCheck = {0, 4, 8};
-
-        return checkLine(indicesToCheck, symbol);
-    }
-
-    private boolean checkColumn(int column, char symbol) {
-        int first = column;
-        int second = first+3;
-        int third = second+3;
-
-        int[] indicesToCheck = {first, second, third};
-
-        return checkLine(indicesToCheck, symbol);
-    }
-
-    private boolean checkRow(int row, char symbol) {
-        int first = row*3;
-        int second = first+1;
-        int third = second+1;
-
-        int[] indicesToCheck = {first, second, third};
-
-        return checkLine(indicesToCheck, symbol);
-    }
-
-    private boolean checkLine(int[] indicesToCheck, char symbol) {
-        boolean result = true;
-
-        for (int index : indicesToCheck) {
-            result = result && gameState[index] == symbol;
-        }
-
-        return result;
-    }
-
     public boolean isFilled() {
         for (char c : gameState) {
             if (c == ' ') {
@@ -112,5 +52,65 @@ public class Board {
         }
 
         return false;
+    }
+
+    private boolean checkForWin(int location, char symbol) {
+        int index = location-1;
+        int row = index/3;
+        int column = index%3;
+
+        boolean win;
+
+        win = checkRow(row, symbol);
+        win = win || checkColumn(column, symbol);
+        if (row == column) {
+            win = win || checkDiagonal1(symbol);
+        }
+        if (row+column == 2) {
+            win = win || checkDiagonal2(symbol);
+        }
+        return win;
+    }
+
+    private boolean checkLineMatchesSymbol(int[] indicesToCheck, char symbol) {
+        boolean result = true;
+
+        for (int index : indicesToCheck) {
+            result = result && gameState[index] == symbol;
+        }
+
+        return result;
+    }
+
+    private boolean checkRow(int row, char symbol) {
+        int first = row*3;
+        int second = first+1;
+        int third = second+1;
+
+        int[] indicesToCheck = {first, second, third};
+
+        return checkLineMatchesSymbol(indicesToCheck, symbol);
+    }
+
+    private boolean checkColumn(int column, char symbol) {
+        int first = column;
+        int second = first+3;
+        int third = second+3;
+
+        int[] indicesToCheck = {first, second, third};
+
+        return checkLineMatchesSymbol(indicesToCheck, symbol);
+    }
+
+    private boolean checkDiagonal1(char symbol) {
+        int[] indicesToCheck = {0, 4, 8};
+
+        return checkLineMatchesSymbol(indicesToCheck, symbol);
+    }
+
+    private boolean checkDiagonal2(char symbol) {
+        int[] indicesToCheck = {2, 4, 6};
+
+        return checkLineMatchesSymbol(indicesToCheck, symbol);
     }
 }
